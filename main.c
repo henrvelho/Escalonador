53dc139f82b90450b386ad41c7021a418dd5fdf6
henrvelho <henrvelho@gmail.com>
2015-11-11 15:07:53 -0200

    Update main.c

--- main.c
+++ main.c
@@ -6,6 +6,17 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	
+	
+// contadores;
+int i=0;
 	imprime();
+	
+	do {
+		printf("Bem vindo ao escalonador de processos");
+		printf("Digite os processos ");
+		printf("em formato: <id, tempo de chegada, tempo de execução e tipo>");
+	}
+	
 	return 0;
 }
