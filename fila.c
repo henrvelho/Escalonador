11fc0f2c6bcd5e4294e85529d4bd4de08185119b
henrvelho <henrvelho@gmail.com>
2015-11-11 14:59:16 -0200

    Update fila.c

--- fila.c
+++ fila.c
@@ -2,5 +2,12 @@
 #include <stdio.h>
 
 void imprime(){
-	printf("Okodkjoaskjdçsa");
+	printf("OkodkjoaskjdÃ§sa");
+}
+
+Pilha* pilha_cria (void)
+{
+Pilha* p = (Pilha*) malloc(sizeof(Pilha));
+p->prim = NULL;
+return p;
 }
